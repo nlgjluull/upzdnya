097509c13c95eb75b1e7c4a5422538d5ac127606
Your Name <you@example.com>
2025-03-21 11:36:51 +0300

    z3

---
+++ app/src/main/java/com/example/zanyat1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.zanyat1
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import  android.content.Intent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
